60b0a32f29df2f61e8ec3194786bbcbbcbba67f8
Serhii Holub <serhii.holub@valtech.com>
2024-02-21 16:20:18 +0200

    Alternative Props Syntaxes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import reactImg from "./assets/react-core-concepts.png";
 import componentsImg from "./assets/components.png";
+import { CORE_CONCEPTS } from "./data";
 
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
@@ -22,12 +23,22 @@ function Header() {
   );
 }
 
-function CoreConcept(props) {
+/* function CoreConcept(props) {
   return (
     <li>
       <img src={props.image} alt={props.title}></img>
       <h3>{props.title}</h3>
       <p>{props.description}</p>
+    </li>
+  );
+} */
+
+function CoreConcept({ image, title, description }) {
+  return (
+    <li>
+      <img src={image} alt={title}></img>
+      <h3>{title}</h3>
+      <p>{description}</p>
     </li>
   );
 }
@@ -40,14 +51,16 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept
-              title="Components"
-              description="The core UI building block."
-              image={componentsImg}
-            />
-            <CoreConcept />
-            <CoreConcept />
-            <CoreConcept />
+            {
+              <CoreConcept
+                title={CORE_CONCEPTS[0].title}
+                description={CORE_CONCEPTS[0].description}
+                image={CORE_CONCEPTS[0].image}
+              />
+            }
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
       </main>
