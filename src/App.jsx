f98a7c738bf6868c6db7b8f373688ee6bbb7178e
Serhii Holub <serhii.holub@valtech.com>
2024-02-26 17:55:56 +0200

    Deriving & Outputting Data Based on State

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
-import { CORE_CONCEPTS } from "./data";
+import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept/CoreConcept.jsx";
 import TabButton from "./components/TabButton/TabButton.jsx";
+import { EXAMPLES } from "./data.js";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState("Please click a button");
-  
+  const [selectedTopic, setSelectedTopic] = useState('components');
+
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton);
   }
@@ -40,6 +41,13 @@ function App() {
             <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
           {selectedTopic}
         </section>
       </main>
